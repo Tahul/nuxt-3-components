8087cd7c49362603006b7530adce6555e71f280b
Yaël Guilloux <yael.guilloux@gmail.com>
2022-02-09 22:49:25 +0100

    chore(init): first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+html {
+    @apply w-full h-full;
+}
+
+body {
+    @apply bg-black w-full h-full flex items-center justify-center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-8xl text-white">
+    Hello Nuxt3!
+
+    <TestComponent />
+  </div>
+</template>
